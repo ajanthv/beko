8c7d27abae944adc20f8a9980fc22672559600ad
Ajanth Vijeyakanthan <ajanth.v@eyepax.com>
2017-01-22 04:55:28 +0530

    css

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -10,7 +10,7 @@
     <div class="container-fluid">
         <div class="navbar-header">
             <a class="navbar-brand" href="#">
-                BEKO ADMIN
+                BEKO <span>ADMIN</span>
             </a>
         </div>
     </div>
