b4357e4fa0ed10f511232bf736ffdf7ce0d3839b
Ajanth Vijeyakanthan <ajanth.v@eyepax.com>
2017-01-22 04:41:23 +0530

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,6 @@
         <!-- nav -->
         <nav class="navbar navbar-default">
             <div class="container-fluid">
-                <!-- Brand and toggle get grouped for better mobile display -->
                 <div class="navbar-header">
                     <a class="navbar-brand" href="#">BEKO <span>PROMOTIONS</span></a>
                 </div>
